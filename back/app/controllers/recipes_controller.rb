03e172ed6ff4131838ace0e16c29a94d7e5ad492
my_name <my_name@example.com>
2020-08-31 15:47:11 +0900

    #1 Recipe API

---
+++ back/app/controllers/recipes_controller.rb
@@ -0,0 +1,45 @@
+class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :update, :destroy]
+    before_action :authenticate_user!
+
+  def index
+    recipes = current_user.recipes
+    render json: { status: 'SUCCESS', message: 'loaded recipes', data: recipes }
+  end
+
+  def show
+    render json: { status: 'SUCCESS', message: 'loaded recipes', data: @recipe }
+  end
+
+  def create
+    recipe = current_user.recipes.new(recipe_params)
+    if recipe.save
+      render json: { status: 'SUCCESS', data: recipe }
+    else
+      render json: { status: 'ERROR', data: recipe.errors.full_messages }
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    render json: { status: 'SUCCESS', message: 'Deleted the recipe', data: @recipe }
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      render json: { status: 'SUCCESS', message: 'Updated the recipe', data: @recipe }
+    else
+      render json: { status: 'ERROR', message: 'Not updated', data: @recipe.errors }
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :cost, :images)
+  end
+end
--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and 
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :recipes, dependent: :destroy
+  has_one_attached :image
 end
