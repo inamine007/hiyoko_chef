c9aabdb91ec517f9c66943c69f83f20be7ba74a9
my_name <my_name@example.com>
2020-09-02 13:47:23 +0900

    #16 Relationship API

---
+++ back/app/controllers/relationships_controller.rb
@@ -0,0 +1,33 @@
+class RelationshipsController < ApplicationController
+    before_action :set_user, only: [:create, :destroy]
+
+    def index
+        followings = current_user.followings.all
+        followers = current_user.followers.all
+        render json: { status: 'SUCCESS', data_followings: followings, data_followers: followers }
+    end
+
+    def create
+        following = current_user.follow(@user) unless current_user == @user.id
+        if following.save
+            render json: { status: 'SUCCESS', data: following }
+        else
+            render json: { status: 'ERROR', data: following.errors.full_messages }
+        end
+    end
+
+    def destroy
+        following = current_user.unfollow(@user)
+        if following.destroy
+            render json: { status: 'SUCCESS', data: following }
+        else
+            render json: { status: 'ERROR', data: following.errors.full_messages }
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:follow_id])
+    end
+end
